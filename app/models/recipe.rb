5e5dbfe48c230d97c3d27dd30bdfc426a8b1e0ae
drewburns <andrewburns@Andrews-MacBook-Pro.local>
2015-06-11 22:03:12 -0400

    mistake

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,5 +4,5 @@ class Recipe < ActiveRecord::Base
 	validates :name,  presence: true, length: { maximum: 50 }
 	validates :description,  presence: true, length: { maximum: 140 }
 	validates :ingredients,  presence: true, length: { maximum: 300 }
-	validates :directions,  presence: true, length: { maximum: 1000 }
+	validates :instructions,  presence: true, length: { maximum: 1000 }
 end
