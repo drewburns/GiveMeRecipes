f355bd08b41a7003acfd6de211aff1824ed5cdf6
drewburns <andrewburns@Andrews-MacBook-Pro.local>
2015-06-19 14:27:40 -0400

    used foundation to style all pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,14 @@ class UsersController < ApplicationController
 	before_action :authenticate_user!, except: :index
 	def show
 		@user = User.find(params[:id])
-		@meals = Meal.where(user_id: params[:id]).where("for_date > ?", Date.yesterday).reverse
+		@meals = Meal.where(user_id: params[:id]).where("for_date > ?", Date.yesterday)
 		@dates = Meal.new.available_dates(@user)
 		@meal = Meal.new
 	end
 
 	def index
+		if user_signed_in?
+			redirect_to current_user
+		end
 	end
 end
