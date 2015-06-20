292274d525c43a90a890dc5f1fb340f291325664
drewburns <andrewburns@Andrews-MacBook-Pro.local>
2015-06-20 13:38:13 -0400

    fixed ordering of meals in user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 	before_action :authenticate_user!, except: :index
 	def show
 		@user = User.find(params[:id])
-		@meals = Meal.where(user_id: params[:id]).where("for_date > ?", Date.yesterday)
+		@meals = Meal.where(user_id: params[:id]).where("for_date > ?", Date.yesterday).sort_by{|e| e[:for_date]}
 		@dates = Meal.new.available_dates(current_user)
 		@meal = Meal.new
 	end
